d093665dac310d686bc38ba01e5acce6a2f5f601
Ihsan Ullah <68999003+ihsan-buneri@users.noreply.github.com>
2024-08-08 21:15:19 +0500

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product.route.js");
 const app = express();
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
-});
 
-app.get("/", (req, res) => {
-	res.send("Hello From Node API");
-});
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+
+app.use("/api/products", productRoute);
+
+mongoose
+	.connect(
+		"mongodb+srv://Ihsan:tcqjECYKvYaJy9mh@backenddb.jojmu.mongodb.net/Node-Js-Crud?retryWrites=true&w=majority&appName=BackendDB"
+	)
+	.then(() => {
+		console.log("Connected to the database");
+		app.listen(3000, () => {
+			console.log("Server is running on port 3000");
+		});
+	})
+	.catch(() => {
+		console.log("Connection failed");
+	});
